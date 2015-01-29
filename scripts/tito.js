b6325fbe7212fd8c48d65c433f5382e0fd31777e
Anthony Johnson <aj@ohess.org>
2015-01-28 18:06:43 -0800

    Add tito integration

---
+++ scripts/tito.js
@@ -0,0 +1,17 @@
+/* Tito integration */
+
+module.exports = function (robot) {
+  robot.router.post('/services/tito', function (req, res) {
+    try {
+      var attendee = req.body.payload;
+      robot.messageRoom(
+        'conference',
+        attendee.name + '(' + attendee.email + ') bought a ' + attendee.release + ' ticket'
+        )
+        console.dir(req.body);
+    } catch (error) {
+      console.log(error);
+    }
+    res.send('OK');
+  });
+};
