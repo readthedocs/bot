6894617d6297bb666feb793a6d5ece631c39c322
Anthony Johnson <aj@ohess.org>
2015-01-30 19:25:00 -0800

    More motiviation

--- scripts/motivation.js
+++ scripts/motivation.js
@@ -1,8 +1,24 @@
 module.exports = function (robot) {
-    robot.hear(/!m (.*)$/i, function (msg) {
-        msg.send("You're doing great work, " + msg.match[1] + "!");
+    robot.hear(/!m(.*)$/i, function (msg) {
+        var messages = ["You're doing great work, {0}!",
+                        "Keep up the great work, {0}!",
+                        "Nice job, {0}!"];
+        var target = msg.match[1].trimLeft();
+        if (target == '') {
+            target = 'everyone';
+        }
+        var message = messages[Math.floor(Math.random() * messages.length)];
+        msg.send(message.replace('{0}', target));
     });
-    robot.hear(/!dm (.*)$/i, function (msg) {
-        msg.send("You're doing awful work, " + msg.match[1] + "!");
+    robot.hear(/!dm(.*)$/i, function (msg) {
+        var messages = ["You're doing awful work, {0}!",
+                        "What do you think you are doing, {0}?!",
+                        "Get your shit together, {0}!"];
+        var target = msg.match[1].trimLeft();
+        if (target == '') {
+            target = 'everyone';
+        }
+        var message = messages[Math.floor(Math.random() * messages.length)];
+        msg.send(message.replace('{0}', target));
     });
 };
