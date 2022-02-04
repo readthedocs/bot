d4ce6a4d8c082d0cb892e9569a4a54fff4a91f2a
Anthony <aj@ohess.org>
2022-02-04 20:31:14 +0000

    Discord bot working

--- scripts/discord.js
+++ scripts/discord.js
@@ -1,38 +1,35 @@
-const Discord = require('discord.js');
+const { Client, Intents } = require('discord.js');
 const process = require('process'); 
 
-
 module.exports = function (robot) {
-  const bot = new Discord.Client();
+  const bot = new Client({
+    intents: [
+      Intents.FLAGS.GUILDS,
+      Intents.FLAGS.GUILD_VOICE_STATES,
+    ]
+  });
+  const slack_room = "testing";
 
   bot.login(process.env.DISCORD_BOT_TOKEN);
 
   bot.once('ready', () =>{
-    robot.messageRoom('testing', `Bot ${bot.user.tag} ready!`);
+    console.log('Bot ready.');
   })
 
-  /*
-  robot.router.post('/services/circleci', function (req, res) {
-    robot.messageRoom('testing', 'TEST');
-    res.send('OK');
-  });
-  */
-};
-
-/*
-
-bot.on('voiceStateUpdate', (oldMember, newMember) => {
-    let newUserChannel = newMember.voiceChannelID
-    let oldUserChannel = oldMember.voiceChannelID
-
-    if(newUserChannel === 712677767333937284) {
-        // User Joins a voice channel
-        console.log("Joined VC1")
-
-    } else if(newUserChannel !== 712677767333937284){
-        // User leaves a voice channel
-        console.log("Left VC1")
-
+  bot.on('voiceStateUpdate', (oldMember, newMember) => {
+    if (newMember.channel) {
+      // Joined
+      robot.messageRoom(
+        slack_room,
+        `${newMember.member.displayName} joined voice channel "${newMember.channel.name}"`,
+      );
     }
-})
-*/
+    else if (oldMember.channel) {
+      // Parted
+      robot.messageRoom(
+        slack_room,
+        `${oldMember.member.displayName} left "${oldMember.channel.name}"`,
+      );
+    }
+  });
+};
