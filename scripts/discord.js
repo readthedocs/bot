35689480dfd36117b5342fad3cbbe38a9c169624
Anthony <aj@ohess.org>
2022-02-23 21:54:17 +0000

    Don't notify on discord mute, add some smarter notifies to slack

--- scripts/discord.js
+++ scripts/discord.js
@@ -9,6 +9,7 @@ module.exports = function (robot) {
     ]
   });
   const slack_room = "testing";
+  var channels = {};
 
   bot.login(process.env.DISCORD_BOT_TOKEN);
 
@@ -16,20 +17,45 @@ module.exports = function (robot) {
     console.log('Bot ready.');
   })
 
+  const announce_members = (members, channel) => {
+    if (members.length) {
+      const member_list = members.join(', ');
+      const member_verb = (members.length == 1) ? 'is' : 'are';
+      robot.messageRoom(
+        slack_room,
+        `${member_list} ${member_verb} in voice channel "${channel.name}"`,
+      );
+    }
+    else {
+      robot.messageRoom(
+        slack_room,
+        `Everyone left voice channel "${channel.name}"`,
+      );        
+    }
+  }
+
+  const update_channel = (channel) => {
+    if (!channel) {
+      return false;
+    }
+
+    let members = [];
+    if (channel.members) {
+      members = Array.from(
+        channel.members.mapValues(member => member.displayName).values()
+      );
+    }
+
+    const channel_before = channels[channel.name] || [];
+    if (members.length != channel_before.length) {
+      announce_members(members, channel);
+    }
+
+    channels[channel.name] = members;
+  }
+  
   bot.on('voiceStateUpdate', (oldMember, newMember) => {
-    if (newMember.channel) {
-      // Joined
-      robot.messageRoom(
-        slack_room,
-        `${newMember.member.displayName} joined voice channel "${newMember.channel.name}"`,
-      );
-    }
-    else if (oldMember.channel) {
-      // Parted
-      robot.messageRoom(
-        slack_room,
-        `${oldMember.member.displayName} left "${oldMember.channel.name}"`,
-      );
-    }
+    update_channel(oldMember.channel);
+    update_channel(newMember.channel);  
   });
 };
